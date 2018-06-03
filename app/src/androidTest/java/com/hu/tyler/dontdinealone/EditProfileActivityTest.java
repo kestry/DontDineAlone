49e6975e3ad96b3bf92dedd90fb2bc7112beb42e
codyscode <codyhc520@gmail.com>
2018-06-02 21:04:16 -0700

    Fixed cancel button falling off edge of screen so unit test would pass.

--- app/src/androidTest/java/com/hu/tyler/dontdinealone/EditProfileActivityTest.java
+++ app/src/androidTest/java/com/hu/tyler/dontdinealone/EditProfileActivityTest.java
@@ -1,11 +1,26 @@
 package com.hu.tyler.dontdinealone;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.support.test.rule.ActivityTestRule;
 import android.view.View;
+import android.widget.EditText;
+
+import com.hu.tyler.dontdinealone.data.Entity;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.Rule;
+
+import java.util.Map;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 public class EditProfileActivityTest {
@@ -14,6 +29,9 @@ public class EditProfileActivityTest {
     public ActivityTestRule<EditProfileActivity> EditProfileActivityTestRule =
             new ActivityTestRule<EditProfileActivity>(EditProfileActivity.class);
     private EditProfileActivity profileActivity = null;
+    Instrumentation.ActivityMonitor returnToLobbyMonitor =
+            getInstrumentation().addMonitor(LobbyActivity.class.getName(), null, false);
+
 
     @Before
     public void setUp() throws Exception {
@@ -27,7 +45,7 @@ public class EditProfileActivityTest {
 
     @Test
     // Tests to see if all UI features are displayed on the Edit Profile page
-    public void onCreate() {
+    public void testOnCreate() {
         View cancelButton    = profileActivity.findViewById(R.id.buttonEditCancel);
         View okButton        = profileActivity.findViewById(R.id.buttonEditOk);
         View textAnimalSpace = profileActivity.findViewById(R.id.editTextAnimal);
@@ -44,5 +62,41 @@ public class EditProfileActivityTest {
         assertNotNull(textGenderSpace);
         assertNotNull(editNamePrompt);
         assertNotNull(editNameSpace);
+    }
+
+    // Verifies the Cancel button brings you back to the lobby.
+    @Test
+    public void testOKButtonOnClick(){
+        onView(withId(R.id.buttonEditOk)).perform(click());
+        Activity lobbyActivity = getInstrumentation().waitForMonitorWithTimeout(returnToLobbyMonitor, 5000);
+        assertNotNull(lobbyActivity);
+        lobbyActivity.finish();
+    }
+
+    // Verifies the Ok button brings you back to the lobby.
+    @Test
+    public void testCancelButtonOnClick(){
+        onView(withId(R.id.buttonEditCancel)).perform(click());
+        Activity lobbyActivity = getInstrumentation().waitForMonitorWithTimeout(returnToLobbyMonitor, 5000);
+        assertNotNull(lobbyActivity);
+        lobbyActivity.finish();
+    }
+    
+    // Verifies text entered into fields is properly saved to the Entity class.
+    @Test
+    public void testSaveUserInfo(){
+        String name = "Test Name";
+        String gender = "Test Gender";
+        String animal = "Test Animal";
+        // Enters test values into text fields
+        onView(withId(R.id.editTextDisplayName)).perform(replaceText(name));
+        onView(withId(R.id.editTextGender)).perform(replaceText(gender));
+        onView(withId(R.id.editTextAnimal)).perform(replaceText(animal));
+        // Saves the entered info
+        onView(withId(R.id.buttonEditOk)).perform(click());
+        // Verifies the entered info was saved to the Entity class
+        assertEquals(name, Entity.user.getDisplayName());
+        assertEquals(gender, Entity.user.getGender());
+        assertEquals(animal, Entity.user.getAnimal());
     }
 }
